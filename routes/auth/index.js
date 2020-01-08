f62e82fc0bdf700b98cfed7fed9967042b5157f0
Sanjari Alfian <sanjari.alfian.01@gmail.com>
2020-01-08 09:15:18 +0700

    Update index.js

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
 
 router.get('/', (req, res, next) => {
     res.redirect('/auth/in');
