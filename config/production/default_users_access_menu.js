368a7abee325d33a4a7153f8a31206a66663e264
Sanjari Alfian <sanjari.alfian.01@gmail.com>
2020-01-12 19:22:53 +0700

    Update default_users_access_menu.js

--- config/production/default_users_access_menu.js
+++ config/production/default_users_access_menu.js
@@ -12,67 +12,55 @@ exports.seed = function (knex) {
         {
           id: 2,
           users_role_id: 1,
-          users_menu_id: 2,
+          users_menu_id: 3,
           created_at: new Date()
         },
         {
           id: 3,
           users_role_id: 1,
-          users_menu_id: 3,
+          users_menu_id: 4,
           created_at: new Date()
         },
         {
           id: 4,
           users_role_id: 1,
-          users_menu_id: 4,
+          users_menu_id: 5,
           created_at: new Date()
         },
         {
           id: 5,
           users_role_id: 1,
-          users_menu_id: 5,
-          created_at: new Date()
-        },
-        {
-          id: 6,
-          users_role_id: 1,
           users_menu_id: 6,
           created_at: new Date()
         },
         {
-          id: 7,
-          users_role_id: 2,
-          users_menu_id: 1,
-          created_at: new Date()
-        },
-        {
-          id: 8,
+          id: 6,
           users_role_id: 2,
           users_menu_id: 2,
           created_at: new Date()
         },
         {
-          id: 9,
+          id: 7,
           users_role_id: 2,
           users_menu_id: 3,
           created_at: new Date()
         },
         {
-          id: 10,
+          id: 8,
           users_role_id: 2,
           users_menu_id: 4,
           created_at: new Date()
         },
         {
-          id: 11,
+          id: 9,
           users_role_id: 2,
           users_menu_id: 5,
           created_at: new Date()
         },
         {
-          id: 12,
+          id: 10,
           users_role_id: 2,
-          users_menu_id: 6,
+          users_menu_id: 7,
           created_at: new Date()
         }
       ]);
